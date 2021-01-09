1e03e4d58b62b153bc0420e6e1e73f3e15625cf8
Vassilis <vassilispapadop@gmail.com>
2021-01-09 11:31:33 +0200

    stochastic environment schematic

---
+++ a_star.py
@@ -0,0 +1,20 @@
+import networkx as nx
+from networkx.classes.coreviews import AdjacencyView
+import matplotlib.pyplot as plt
+import pydot
+from networkx.drawing.nx_pydot import graphviz_layout
+
+def construct_graph():
+    #  - - - - - - - - - - - - - -
+    # |(1,3), (2,3), (3,3), (4,1)|
+    # |(1,2), (X,X), (3,2), (X,X)|
+    # |(1,1), (2,1), (3,1), (4,1)|
+    #  - - - - - - - - - - - - - -
+    print('')
+
+def main():
+    print("Construct graph")
+    construct_graph()
+
+if __name__ == "__main__":
+    main()
