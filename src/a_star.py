3e80c0c88657c67b67c74bffd6e0962498183496
Vassilis <vassilispapadop@gmail.com>
2021-02-22 13:47:57 +0200

    commit

---
+++ src/a_star.py
@@ -0,0 +1,130 @@
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(grid, start, end, heuristic):
+    """Returns a list of tuples as a path from the given start to the given end in the given grid"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f > current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        # for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+        # Adjacent squares, diagonal move is not allowed
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: 
+    
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(grid) - 1) or node_position[0] < 0 or node_position[1] > (len(grid[len(grid)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if grid[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            # for closed_child in closed_list:
+            #     if child == closed_child:
+            #         continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            # Manhattan distance
+            # child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.h = heuristic[child.position[0]][child.position[1]]
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            # for open_node in open_list:
+            #     if child == open_node and child.g > open_node.g:
+            #         continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+
+def main():
+    # 1-indicates wall
+    grid = [[0,0,0,0],
+            [0,1,0,0],
+            [0,0,0,0]]
+
+    values9 = [[0.509, 0.650, 0.795,  1.000],
+                [0.399, 0.000, 0.486, -1.000],
+                [0.296, 0.254, 0.345, 0.130]]
+
+    values6 = [[ 0.066, 0.215, 0.477, 1.000],
+                [-0.009, 0.000, 0.137, -1.000],
+                [-0.050, -0.035, 0.019, -0.085]]
+
+    values2 = [[-0.045, -0.021, 0.122, 1.000],
+                [-0.049, 0.000, -0.041, -1.000],
+                [-0.050, -0.050, -0.049, -0.050]]
+
+    start = (2, 0)
+    end = (0, 3)
+
+    path = astar(grid, start, end, values9)
+    print(path)
+
+
+if __name__ == '__main__':
+    main()
