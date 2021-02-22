d1e702b59f6ba54362a6418ea6feb6e3442aaf19
Vassilis <vassilispapadop@gmail.com>
2021-02-22 14:44:37 +0200

    made simply grid with pands

--- q-learning/q_learning.py
+++ q-learning/q_learning.py
@@ -1,1 +1,47 @@
 import numpy as np
+import pandas as pd
+# rewards
+alpha = 1
+beta = 1
+delta = -1
+zeta = -1
+
+
+class Agent():
+    def __init__(self, name, e_greedy):
+        self.name = name
+        self.e_greedy = e_greedy
+
+    def choose_action(self):
+        action = None
+        if np.random.rand(1) > 0.49:
+            action = 'A'
+        else:
+            action = 'D'
+            
+        return action
+
+
+if __name__ == "__main__":
+    episodes = 10
+    #grid
+    grid=pd.DataFrame({
+                        "A":[(alpha, beta),(delta, zeta)], 
+                        "D":[(zeta, delta),(beta, alpha)]
+                    }) 
+    grid.index = ['A','D']
+
+    player1 = Agent('col_player', 1)
+    player2 = Agent('row_player', 1)
+    i = episodes
+    while i > 0:
+        choice1 = player1.choose_action()
+        choice2 = player2.choose_action()
+        print("col player chose {}, row player chose {} -> reward {}".format(choice1, choice2, grid[choice1][choice2]))
+        i -= 1
+
+
+
+
+
+
